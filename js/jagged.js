fcc580765fc9b66cf4f20396e1673b6a8a666564
en3m3 <71306799+en3m3@users.noreply.github.com>
2021-12-15 12:34:10 -0700

    fixes

---
+++ js/jagged.js
@@ -0,0 +1,40 @@
+[
+    {
+        "character":"Mary Jane Healy",
+        "actorName":"Heidi Blickenstaff",
+        "image_link": "https://jaggedlittlepill.com/wp-content/uploads/2021/09/Heidi-Blickenstaff.jpg",
+        "bio":"ELIZABETH STANLEY (Mary Jane Healy) recently starred in the world premiere of the new musical Jagged Little Pill by Alanis Morissette, Diablo Cody, and Glen Ballard, directed by Diane Paulus at A.R.T. She previously appeared on Broadway in the revival of On the Town (Drama Desk Nomination),        Million Dollar Quartet, Cry-Baby, and the Tony Award-Winning revival of Company as well as the First National Tours of The Bridges of Madison County and Xanadu. On television, Elizabeth can be seen in “NOS4A2” (upcoming), “FBI,” “The Get Down”, “The Affair,” “Black Box,” “Made in Jersey,” “Fringe,” “The Chappelle Show,” and “PBS Great Performances – Company.”"
+    },
+    {
+        "character":"Frankie Healy",
+        "actorName":"Morgan Dudley",
+        "image_link":"https://jaggedlittlepill.com/wp-content/uploads/2021/09/Morgan-Dudley.jpg",
+        "bio":"Originally from Hiram, GA, Morgan Lynee Dudley is best known for her work on screen and on stage. She most recently appeared in the 2020 musical Netflix film The Prom directed by Ryan Murphy.Some of her other notable credits include an appearance in the Nickelodeon hit series Henry Danger, and a feature in season 3 of Scenebot Stage Live where she performed “Burn” from Hamilton."
+    },
+    {
+        "character":"Bella",
+        "actorName":"Kathryn Gallagher",
+        "image_link":"https://jaggedlittlepill.com/wp-content/uploads/2019/08/Kathryn-Gallagher1.jpg",
+        "bio":"",
+    },{
+        "character":"Nick Healy",
+        "actorName":"Derek Klena",
+        "image_link":"https://jaggedlittlepill.com/wp-content/uploads/2019/08/DerekKlena1.jpg",
+        "bio":"Broadway: Anastasia (Dmitry, OBC), The Bridges of Madison County (OBC), Wicked (Fiyero, 10th anniversary company). Off-Broadway: Dogfight (Second Stage), Carrie (MCC). TV: “The Code” (CBS), “Unbreakable Kimmy Schmidt” (Netflix), “Quantico” (ABC), “Last Week Tonight With John Oliver” (HBO), “Blue Bloods” (CBS), “Law & Order: SVU” (NBC).",
+    },{
+        "character":"Steve Healy",
+        "actorName":"Sean Allan Krill",
+        "image_link":"https://jaggedlittlepill.com/wp-content/uploads/2019/08/Sean-Allan-Krill2.jpeg",
+        "bio":" Broadway: Honeymoon in Vegas, On A Clear Day You Can See Forever, Mamma Mia! Off-Broadway: Joan of Arc: Into the Fire, Brother/Sister Plays (Public), Hit The Wall (Barrow Street), A Civil War Christmas (NYTW). Tours: Thoroughly Modern Millie (Trevor Graydon – Leon Rabin & Carbonell Award nominations), Mamma Mia! Regional: Jagged Little Pill (A.R.T.), Chess (Kennedy Center), Sense and Sensibility (Colonel Brandon – Chicago Shakespeare & Old Globe, Jeff Award nomination and Craig Noel Award recipient), Hamlet, The Comedy of Errors (Chicago Shakespeare), Sideways (La Jolla), Hot L Baltimore (Steppenwolf) TV: “Search Party,” “Godfather of Harlem,” “Mr. Robot”"
+    },{
+        "character":"Jo",
+        "actorName":"Lauren Patten",
+        "image_link":"https://jaggedlittlepill.com/wp-content/uploads/2019/08/LaurenPatten1.jpg",
+        "bio":" LAUREN PATTEN (Jo) originated the role of Jo in American Repertory Theater’s production of Jagged Little Pill. Other theatre credits include: the Broadway production of Fun Home, The Wolves (Obie and Drama Desk winner), and Steven Levenson’s Days of Rage. Film and television credits include: “Blue Bloods,” “The Good Fight,” “Succession,” The Big Sick"
+    },{
+        "character":"Mary Jane Healy",
+        "actorName":"Elizabeth Stanley",
+        "image_link":"https://jaggedlittlepill.com/wp-content/uploads/2019/08/Elizabeth1.jpg",
+        "bio":"Broadway: Something Rotten! (Bea), Outer Critics and Grammy nominations, The Addams Family (Alice), Disney’s The Little Mermaid (Ursula), The Full Monty, and [title of show] (Heidi).  Heidi created the role of Katherine in Freaky Friday for Disney Channel and Disney on Broadway. Other New York: City Center’s Encores! The Most Happy Fella (Cleo), The Vineyard Theatre: Now. Here. This. and [title of show]. Favorite Regional: Weston Playhouse: Next to Normal (Diana), Kennedy Center, Signature Theatre and PBS: “First You Dream: The Music Of Kander And Ebb”, and Ford’s Theatre: Meet John Doe (Ann Mitchell – Helen Hayes Award.)"
+    }
+]
