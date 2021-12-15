cd404aaa776c053788c55f2e46fec23fb4a336d4
lpomazal <65213842+lpomazal@users.noreply.github.com>
2021-12-15 09:49:45 -0800

    workin20211215

---
+++ moulin.js
@@ -0,0 +1,55 @@
+{"CastList":[
+    {
+        "character":"Satine",
+        "actorName":"Natalie Mendoza",
+        "img":"https://moulinrougemusical.com/wp-content/uploads/2021/07/Natalie_Mendoza_headshot_400x400.jpg",
+        "bio":"Classically Trained: Bristol Old Vic (UK), La Sorbonne (Paris), Playhouse West (US). Broadway: Spider-Man: Turn Off the Dark. Stage: Here Lies Love (National Theatre), Miss Saigon (West End), Arms of Fire (Chester Theatre Company), The Music of Andrew Lloyd Webber Concert Tour (Australia/Asia Tour), Les Misérables, CATS (Australia Tour), Sweet Charity (Australia Capitol Theatre). Television: “Jack Ryan” (Amazon), “Hotel Babylon” (BBC One), “South Pacific” (ABC), “McDonald & Dodds” (ITV), “Holby City”(BBC),“Blue Bloods”(CBS). Film:Leos Carax’s Annette, The Descent, The Descent: 2, Moulin Rouge!, Code 46, The Great Raid, Horseplay. LA’s 72hr Film Festival award winner for her film, Crazy. Natalie offers her performance to the God that dwells in all and the Divine Mother of all Creation." 
+    },
+    {
+        "character":"Christian",
+        "actorName":"Aaron Tveit",
+        "img":"https://moulinrougemusical.com/wp-content/uploads/2019/05/cast_aaron_tveit-2.jpg",
+        "bio":"Broadway: Catch Me If You Can, Next to Normal, Wicked, Hair­spray. Stage: Assassins (West End), Rent (Hollywood Bowl). TV: “Graceland” (USA), “BrainDead” (CBS), “Grease Live!” (FOX), “The Code,” “The Good Fight,” “Gossip Girl.” Film: Enjolras in the Oscar-winning Les Mi­sérables, Out of Blue (TIFF), Created Equal, Undrafted, Better Off Single, Big Sky, Howl, and others. Concerts: NYC: Webster Hall, Irving Plaza, LCT American Songbook. Tours: Paramount N.Y., House of Blues, Boston, San Diego; The Belasco: L.A.; Barns at Wolf Trap: DC; The Vets: RI, San Francisco, Las Vegas, Philly. Recordings: Live Album: “The Radio in My Head.”"
+    },
+    {
+        "character":"Harold Zidler",
+        "actorName":"Danny Burstein",
+        "img":"https://moulinrougemusical.com/wp-content/uploads/2018/06/cast_danny_burstein-1.jpg",
+        "bio":"Eighteen Broadway shows including My Fair Lady, Fiddler on the Roof, Cabaret, Golden Boy, Follies, South Pacific, The Drowsy Chaperone, Saint Joan, The Seagull, Titanic and Company. Off-Broadway includes Describe the Night, A Midsummer Night’s Dream, Talley’s Folly, Mrs. Farnsworth, Psych, All in the Timing, Merrily We Roll Along. Film/TV includes Indignation, The Family Fang, Transamerica, Affluenza, Nor’easter, The Sounding, “Madam Secretary,” “Fosse/Verdon,” “Boardwalk Empire,” “Louie,” “Absolutely Fabulous,” “Law & Order,” “The Good Wife,” “The Blacklist,” “Elementary.” Metropolitan Opera: Die Fledermaus. He’s received two Drama Desk Awards, three Outer Critics Circle Awards, six Tony Award nominations and two Grammy Award nominations."
+    },{
+        "character":"Toulouse-Lautrec",
+        "actorName":"Sahr Ngaujah",
+        "img":"https://moulinrougemusical.com/wp-content/uploads/2018/06/cast_sahr_ngaujah.jpg",
+        "bio":"Sahr Ngaujah created the title role in FELA! earning him Obie and Theater World awards, as well as Tony, Olivier, and Grammy nominations. Off-Broadway: Boesman and Lena, Mlima’s Tale (Lortel and Drama League nominations), Master Harold…and the Boys, Signature Plays and The Painted Rocks at Revolver Creek. Select TV/film: Patti Cake$, “High Maintenance,” “Law & Order: SVU,” “The Good Wife,” “Last Resort,” Freeheld, “Luke Cage,” “Overwatch,” Doomfist."
+    },{
+        "character":"The Duke",
+        "actorName":"Tam Mutu",
+        "img":"https://moulinrougemusical.com/wp-content/uploads/2018/06/cast_tam_mutu-2.jpg",
+        "bio":"New York: Hey, Look Me Over! and The New Yorkers (Encores!); Doctor Zhivago (Broadway). Boston: Moulin Rouge! The Musical. U.K.: City of Angels (Donmar);Les Misérables (West End); The Royal Hunt of the Sun and Love’s Labour’s Lost (NT); Romeo and Juliet and King Lear (RSC). Seattle: The Secret Garden (5th Avenue Theatre). Toronto: Chess (Princess of Wales). TV: “Medici,” “Sherlock,” “Blue Bloods.” Film: Mary Poppins Returns."
+    },{
+        "character":"Santiago",
+        "actorName":"Ricky Rojas",
+        "img":"https://moulinrougemusical.com/wp-content/uploads/2018/06/cast_ricky_rojas.jpg",
+        "bio":"Chilean-born, Australian-raised. Works include Burn the Floor (Longacre), Moulin Rouge! The Musical (Colonial), Sister Act The Musical, Tonight’s the Night: Rod Stewart Musical, Grease, Burn the Floor (Shaftesbury), Flashdance The Musical, The Pirates of Penzance, Joseph and the Amazing Technicolor Dreamcoat (Adelphi), Fame: The Musical (Aldwych), and The Buddy Holly Story (Novello)."
+    },{
+        "character":"Nini",
+        "actorName":"Robyn Hurder",
+        "img":"https://moulinrougemusical.com/wp-content/uploads/2019/05/cast_robyn_hurder-2.jpg",
+        "bio":"Broadway: Nice Work If You Can Get It (Jeannie), Grease (Marty), Chicago (Roxie u/s), The Wedding Singer (Holly u/s), Chitty Chitty Bang Bang (Violet). NYCC/Encores!: A Chorus Line (Cassie), The New Yorkers (Lola), Paint Your Wagon (Cherry). Tours: A Chorus Line (Cassie), Starlight Express (Pearl). Regional: Crazy for You (Polly); STC & 5th Ave: Kiss Me, Kate (Lois) *Helen Hayes & *Emery Battis Award recipient, *Gregory Award nominee."
+    },{
+        "character":"La Chocolat",
+        "actorName":"Jacqeline B. Arnold",
+        "img":"https://moulinrougemusical.com/wp-content/uploads/2018/06/Jacqueline-B.-Arnold.jpg",
+        "bio":"Jacqueline B. Arnold is honored to be a part of this innovative and iconic production. Broadway: Priscilla Queen of the Desert. National tours: Bette Midler Harlette, We Will Rock You, Hairspray, Rent. “Endless gratitude to my forever partner Shannon, for your undying love and encouragement.”"
+    },{
+        "character":"Arabia",
+        "actorName":"Holly James",
+        "img":"https://moulinrougemusical.com/wp-content/uploads/2018/06/big-Holly-James-Color.jpg",
+        "bio":"Broad­way: American Psycho. Chicago: Hamilton. West End: Women on the Verge of a Nervous Breakdown; The Body­guard; Kiss Me, Kate; Hairspray; Hair; Shoes; Porgy and Bess; Peter Pan; Wonderland; Cats; Fame; Starlight Express; The Genius of Ray Charles; Sleeping Beauty; Kirov Ballet. Training: Urdang Academy, London."
+    },{
+        "character":"Babydoll",
+        "actorName":"Jeigh Madjus",
+        "img":"https://moulinrougemusical.com/wp-content/uploads/2018/06/Jeigh-Madjus.jpg",
+        "bio":"Broadway debut! Jeigh has worked internationally as a waiter, construction worker, ayurvedic spa receptionist, and performer. Here Lies Love (The Pub­lic/SRT), La Cage aux Folles (1st national). Film: Servitude. “Thanks to Alex, Justin, Sonya, John, and my family.”"
+    }
+]}
---
+++ tina.js
@@ -0,0 +1,102 @@
+"CastList":[
+    {
+        "character":"Tina Turner",
+        "actorName":"Nkeki Obi-Melekwe",
+        image link https://tinaonbroadway.com/wp-content/uploads/2019/07/TINA-Obi-melekwe-Nkeki-400x400.jpg
+        "bio":"NKEKI OBI-MELEKWE (Tina Turner) made her West End debut in Tina—The Tina Turner Musical as Tina Turner and is making her Broadway debut as Tina. Nkeki can be heard as the new voice of Audi. Other credits: Alice by Heart (MCC). TV: “Bull” (CBS) and “Smilf” (Showtime). BFA, University of Michigan. As always, for Mom & Dad. "
+    },
+    {
+        "character":"Ike Turner",
+        "actorName":"Daniel J. Watts",
+        image link https://tinaonbroadway.com/wp-content/uploads/2019/05/TINA-Watts-Daniel-J-1-400x400.jpg
+        "bio":"DANIEL J. WATTS (Ike) 2020 Tony Nominee. Nine Broadway shows including Hamilton, The Color Purple. Off-Bway: The Last Of The Love Letters; Whorl Inside a Loop; Death Of The Last Black Man In The Whole Entire World; LA Ovation & Barrymore Winner for portrayal of Sammy Davis Jr. Lights Out: Nat King Cole. TV credits: recurs on “The Marvelous Mrs. Maisel,” “The Last OG,” “Blindspot,” “Vinyl.” Creator of Daniel J. Watts’ The Jam. Ted Talker. Elon Grad www.wattswords.com @dwattswords Lova you!"
+    },
+    {
+        "character":"Zelma",
+        "actorName":"Dr. Dawnn Lewis",
+        image link https://tinaonbroadway.com/wp-content/uploads/2019/05/TINA-Lewis-Dawnn-400x400.jpg
+        "bio":"DAWNN LEWIS (Zelma) Multiple BMI, ASCAP, Grammy, NAACP Image awards; CEO of Morning Jewel Inc., A New Day Foundation, Women Songwriters Hall of Fame 2021. TV: “911,” “Veronica Mars,” “The Boys,” “Major Crimes,” “A Different World” (theme song). Theatre: The Wiz, Sister Act, Intimate Apparel, The Tap Dance Kid. Animation: “Star Trek: Lower Decks” (NAACP Image Award Nom.), “Carmen San Diego,” “Karma’s World”, “The Simpsons.”"
+    },{
+        "character":"Gran Gorgeanna",
+        "actorName":"Myra Lucretia Taylor",
+        image link https://tinaonbroadway.com/wp-content/uploads/2019/05/TINA-Taylor-Myra-Lucretia-400x400.jpg
+        "bio":"MYRA LUCRETIA TAYLOR (Gran Georgeanna) Broadway: Tina: The Tina Turner Musical (Tony nominee), Nine, Macbeth, Electra, MuleBone, A Streetcar Named Desire. National Tour: Wicked (Morrible). Off Broadway: School Girls; Or, The African Mean Girls Play (Drama Desk Award, Ensemble), The Tempest (The Public Mobile Unit), Familiar, (Lortel and OCC nominee), The Lucky Ones (Lortel nominee). Film/TV: See You Yesterday, Catfight, “The Hunters,” “Atlanta.” Fox Fellow."
+    },{
+        "character":"Rhonda",
+        "actorName":"Jessica Rush",
+        image link https://tinaonbroadway.com/wp-content/uploads/2019/05/TINA-Rush-Jessica-400x400.jpg
+        "bio":"JESSICA RUSH (Ensemble, Rhonda) Broadway credits include Dear Evan Hansen (Cynthia/ Heidi standby), Summer (Joyce Bogart), Jersey Boys (Lorraine), Gypsy (Louise standby). National tours/regional: Mamma Mia!, Cinderella, Gypsy at Chicago Shakespeare Theatre (Joseph Jefferson Award). Co-hosts top rated podcast @mamastalkinloud (download everywhere!) & owner of bespoke jewelry business @ivyandclarke. Married to Eric Anderson, mom to Elliot. @jessisarush"
+    },{
+        "character":"Young Anna Mae/ Young Alline",
+        "actorName":"Juliet Benn",
+        image link https://tinaonbroadway.com/wp-content/uploads/2021/09/Juliet-Benn-464x464.jpg
+        "bio":"JULIET BENN (u/s Young Anna Mae, Young Alline) So happy and grateful to help tell the amazing Tina Turner story! Broadway: Once on This Island. Big thanks to my family, Lisa Calli, Telsey, Bryan Wade, Gigi, Mehret, Lori. Always dance to the beat of your own drum! @julietmbenn"
+    },{
+        "character":"Phil Spector/ Terry Britten",
+        "actorName":"Steven Booth",
+        image link https://tinaonbroadway.com/wp-content/uploads/2019/05/TINA-Booth-Steven-400x400.jpg
+        "bio":"STEVEN BOOTH (Ensemble, Phil Spector, Terry Britten) Broadway: School of Rock (Ned), Glory Days (Will), Avenue Q (Princeton/Rod u/s). Off-Broadway: Dogfight (Gibbs). National tours: Kinky Boots (Charlie Price), Happy Days (Richie Cunningham). Film/TV: “Modern Love” (Craig). Thanks to Steve Maihack and all my love to Molly and Maezie."
+    },{
+        "character":"Ikette",
+        "actorName":"Ayla Cicconee-Burton",
+        image link https://tinaonbroadway.com/wp-content/uploads/2021/10/Ayla-400x400.jpg
+        "bio":"Ayla is overjoyed to make her Broadway Debut! Credits: The Book Of Mormon (Australia), Dirty Dancing…(Tour). Thanks to Gotham Talent, TGS, TKS, The Telsey Office, the support, trust and love from her friends, family and mostly her mother, Christa. @aylaccb "
+    },{
+        "character":"Raymond King",
+        "actorName":"Gerald Caesar",
+        image link https://tinaonbroadway.com/wp-content/uploads/2019/05/TINA-Ceasar-Gerald-400x400.jpg
+        "bio":"GERALD CAESAR (Ensemble, Raymond) is thrilled to be a part of Tina! Broadway/National Tours: A Bronx Tale (OBC), Choir Boy (OBC), Disney’s The Lion King. Film: Dear Evan Hansen. DGRW, inc. & Robert Stein Management. BFA Elon University."
+    },{
+        "character":"Roger Davies",
+        "actorName":"Chrlie Franklin",
+        image link https://tinaonbroadway.com/wp-content/uploads/2019/05/TINA-Franklin-Charlie-400x400.jpg
+        "bio":"CHARLIE FRANKLIN (Ensemble, Roger Davies) Theatre: The Book of Mormon (Broadway), The Bridges of Madison County (OBC), Parade (Lincoln Center), Big River (Encores!), The Secret Garden (5th Avenue). TV: “Shades of Blue,” “The Loudest Voice,” “Succession”. Pace grad! Thanks KMR, Telsey, fam and DeLaney! @itsCharlieFranklin"
+    },
+    {
+        "character":"Young Craig",
+        "actorName":"Josiah Gaffney",
+        image link https://tinaonbroadway.com/wp-content/uploads/2021/10/Josiah-400x400.jpg
+        "bio":"Born in Brooklyn. Credits: “Saturday Night Live,” commercials, Book audible with Laurie Berkner’s Song and Story Kitchen, as Thelonious Pig and the new voice of Goby in the animated series “Bubble Guppies.” He enjoys basketball, and spending time with family."
+    },
+    {
+        "character":"Craig",
+        "actorName":"Matthew Griffin",
+        image link https://tinaonbroadway.com/wp-content/uploads/2019/05/TINA-Griffin-Matthew-400x400.jpg
+        "bio":"MATTHEW GRIFFIN (Ensemble, Craig) is thrilled to be back in Tina! Credits include: New York City Center’s The New Yorkers, Born For This (Cutler Majestic), Cruel Intentions the Musical, The Outsiders (directed by Liesl Tommy), EPIX’s “Godfather of Harlem”. Thank you, Lord."
+    },{
+        "character":"Aline",
+        "actorName":"Ari Groover",
+        image link https://tinaonbroadway.com/wp-content/uploads/2020/04/400x400_Groover-400x400.jpg
+        "bio":"ARI GROOVER (Ensemble, Alline, Ikette) Broadway: Head Over Heels, Holler If Ya Hear Me. Off-Broadway: Little Shop of Horrors (Ronnette), Bare. Choreography: Burn All Night (A.R.T.). Film: In the Heights (Warner Bros.). TV: “Law & Order: SVU.” Ari is a DJ who goes by Ari Grooves. www.arigrooves.com"
+    },{
+        "character":"Richard Bullock",
+        "actorName":"David Jennings",
+        image link https://tinaonbroadway.com/wp-content/uploads/2019/05/TINA-Jennings-David-400x400.jpg
+        "bio":"DAVID JENNINGS (Ensemble, Richard Bullock) Broadway: Once on This Island, After Midnight, Hands on a Hardbody. London: The Genius of Ray Charles. National: Ragtime (Coalhouse), Kinky Boots (Simon Sr.), Dreamgirls (Curtis), Freaky Friday (Mike), Ain’t Misbehavin’, Waitress. Television: “Blue Bloods,” “The Shield,” “The Sinner,” “Last Week Tonight.” @djmusicbiz"
+    },{
+        "character":"Erwin Bach",
+        "actorName":"Ross Lekites",
+        image link  https://tinaonbroadway.com/wp-content/uploads/2019/05/TINA-Lekites-Ross-400x400.jpg
+        "bio":"ROSS LEKITES (Ensemble, Erwin Bach) Broadway: Frozen (OBC), Kinky Boots. Off-Broadway: Brigadoon (Charlie) City Center Encores! National tours: West Side Story (Tony), Kinky Boots. London: West Side Story (Royal Albert Hall). Concerts: Boston Pops, Philly Pops, San Diego Symphony, Virginia Symphony. For Mandy & Leo. www.Rosslekites.com Insta: @rosslekites"
+    },{
+        "character":"Carpenter",
+        "actorName":"Robert Lenzi",
+        image link https://tinaonbroadway.com/wp-content/uploads/2019/05/TINA-Lenzi-Robert-400x400.jpg
+        "bio":"ROBERT LENZI (Ensemble, Carpenter) Broadway: Tuck Everlasting, South Pacific. Off-Broadway: Dogfight, Hello Again. Select regional: Guthrie, Yale Rep, Chicago Shakes, Alliance. Select film/TV: The Last Airbender, SATC 2, The Happening, The Village, “Monsterland”, “OITNB,” “Kimmy Schmidt,” “The Good Fight,” “Blue Bloods.” CMU grad. @bob.lenzi"
+    },{
+        "character":"Ronnie",
+        "actorName":"Jhardon Dishon Milton",
+        image link https://tinaonbroadway.com/wp-content/uploads/2019/07/Milton-Jhardon-DiShon-Headshot-400x400.jpg
+        "bio":"Excited for Broadway debut! TV: “ABC Mouse” (Age of Learning); “Celebrity Ghost Stories” (A&E). Voiceover: “PAW Patrol” (Nickelodeon). Young Artist Academy member and straight A student. Loves God, soccer, cooking, fun with family/friends, and volunteering. Mission to inspire the world!"
+    },{
+        "character":"Young Aline",
+        "actorName":"Phierce Phoenix",
+        image link https://tinaonbroadway.com/wp-content/uploads/2021/10/PhiercePhoneix-400x400.jpg
+        "bio":"JHARDON DiSHON MILTON (Ensemble, Ronnie) Thank you, God! Broadway: A Bronx Tale, Off- Broadway: The Black Clown, Carnegie Hall. TV: “Blue Bloods.” Upcoming: “Harlem” and “And Just Like That.” Gratitude to Gray Talent, Telsey, Mama, Cree and Cole, VB’s, L6, and family. For my nieces.  @jhardondishon"
+    },{
+        "character":"Young Anna Mae",
+        "actorName":"Skye Dakota Turner",
+        image link https://tinaonbroadway.com/wp-content/uploads/2019/08/TINA-Turner-Skye-Dakota-400x400.png
+        "bio":"SKYE DAKOTA TURNER (Young Anna Mae) is thrilled to be returning Broadway to reprise the role of Anna Mae. Skye has recently been seen as Young Aretha in the biopic Respect. Over the pandemic, Skye had the opportunity to work with “Sesame Street” and the Cleveland Playhouse."
+    }
+]
